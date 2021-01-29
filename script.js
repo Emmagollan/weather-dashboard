853107e4b588b95e3feb9a43c00d90995e0b65df
BBT <emmagollan33@gmail.com>
2021-01-29 17:23:50 -0500

    added JS

---
+++ script.js
@@ -0,0 +1,14 @@
+const cityInputE1 = document.getElementById("city-input");
+const seachBtnE1 = document.getElementById("seach-btn");
+const clearHistoryE1 = document.getElementById("clear-history");
+const cityNameE1 = document.getElementById("city-name");
+const historyE1 = document.getElementById("history");
+const imageE1 = document.getElementById("image");
+const tempE1 = document.getElementById("temperature");
+const humidityE1 = document.getElementById("humidity");
+const windSpeedE1 = document.getElementById("wind-speed");
+const uvIndexE1 = document.getElementById("UV-index");
+
+Function getCurrentWeather(cityName) {
+    let queryURL = "api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid" + c0443b1297dbab0e3849b6173dea9ceb 
+}
